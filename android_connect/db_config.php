d7e6fa03e968058698010956c91a313752da5e03
gilbertwong <lazygamer16@live.com>
2015-03-15 13:38:16 +0800

    changes to ip

--- android_connect/db_config.php
+++ android_connect/db_config.php
@@ -7,5 +7,5 @@
 define('DB_USER', "adminPaQ8nQa"); // db user
 define('DB_PASSWORD', "kWA_f3T8W5tP"); // db password (mention your db password here)
 define('DB_DATABASE', "androidhive"); // database name
-define('DB_SERVER', "myfirstphpapp-androidconnect.rhcloud.com"); // db server
+define('DB_SERVER', "127.3.169.2"); // db server
 ?>
