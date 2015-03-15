4d8453ae60461873937e6d50dbe0c96773e692e4
gilbertwong <lazygamer16@live.com>
2015-03-15 13:12:06 +0800

    adding files

---
+++ android_connect/db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * All database connection variables
+ */
+
+define('DB_USER', "adminPaQ8nQa"); // db user
+define('DB_PASSWORD', "kWA_f3T8W5tP"); // db password (mention your db password here)
+define('DB_DATABASE', "androidhive"); // database name
+define('DB_SERVER', "myfirstphpapp-androidconnect.rhcloud.com"); // db server
+?>
