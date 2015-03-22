b6165c30c9fb9fd74b76d76196fa161471889298
gilbertwong <lazygamer16@live.com>
2015-03-22 13:46:56 +0800

    change the parameters

--- uis/login.php
+++ uis/login.php
@@ -14,7 +14,7 @@
     $password=$_POST['password'];
     
     // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-    $connection = mysql_connect("localhost", "root", "");
+    $connection = mysql_connect("127.3.169.2", "adminPaQ8nQa", "kWA_f3T8W5tP");
     
     // To protect MySQL injection for Security purpose
     $username = stripslashes($username);
