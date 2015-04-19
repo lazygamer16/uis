d9a29eb5f80dae0b9d2509f78f9eebec012de55a
gilbertwong <lazygamer16@live.com>
2015-04-19 19:33:18 +0800

    typofix

--- uis_connect/db_functions.php
+++ uis_connect/db_functions.php
@@ -7,7 +7,7 @@ class DB_Functions {
     //put your code here
     // constructor
     function __construct() {
-        require_once 'db_connect.php';
+        require_once '/db_connect.php';
         // connecting to database
         $this->db = new DB_CONNECT();
         $this->db->connect();
@@ -23,8 +23,9 @@ class DB_Functions {
      */
     public function getUserByEmailAndPassword($email, $password) {
         $result = mysql_query("SELECT * FROM user
-								INNER JOIN student
-									ON student.user_id=user.user_id where user.user_name='$email'") or die(mysql_error());
+		INNER JOIN student
+		ON student.user_id=user.user_id where user.user_name='$email'") or die(mysql_error());
+		
         // check for result 
         $no_of_rows = mysql_num_rows($result);
         if ($no_of_rows > 0) {
