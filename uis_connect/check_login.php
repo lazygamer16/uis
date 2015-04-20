4ac482e03bc5ada7dc962a1c657fc46f51c91210
gilbertwong <lazygamer16@live.com>
2015-04-20 08:00:38 +0800

    replacing with another copy

---
+++ uis_connect/check_login.php
@@ -0,0 +1,49 @@
+<?php
+
+if (isset($_GET['tag']) && $_GET['tag'] != ''){
+    // get tag
+    $tag = $_GET['tag'];
+ 
+    // include db handler
+    require_once '/db_functions.php';
+    $db = new DB_Functions();
+ 
+    // response Array
+    $response = array("tag" => $tag, "error" => FALSE);
+ 
+    // check for tag type
+    if ($tag == 'login') {
+        // Request type is check Login
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+ 
+        // check for user
+        $user = $db->getUserByEmailAndPassword($email, $password);
+        if ($user != false) {
+            // user found
+            $response["error"] = FALSE;
+            $response["uid"] = $user["user_id"];
+			$response["sid"] = $user["student_id"];
+            $response["user"]["name"] = $user["student_name"];
+            $response["user"]["email"] = $user["user_name"];
+            echo json_encode($response);
+        } else {
+            // user not found
+            // echo json with error = 1
+            $response["error"] = TRUE;
+            $response["error_msg"] = "Incorrect email or password!";
+            echo json_encode($response);
+        }
+    } else {
+        // user failed to store
+        $response["error"] = TRUE;
+        $response["error_msg"] = "Unknown 'tag' value. It should be either 'login' or 'register'";
+        echo json_encode($response);
+    }
+} else {
+    $response["error"] = TRUE;
+    $response["error_msg"] = "Required parameter 'tag' is missing!";
+    echo json_encode($response);
+}
+
+?>
