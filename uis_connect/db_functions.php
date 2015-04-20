bf0977f8725e875888a05893df3c3b2f8a71966d
gilbertwong <lazygamer16@live.com>
2015-04-20 08:41:42 +0800

    typo

--- uis_connect/db_functions.php
+++ uis_connect/db_functions.php
@@ -7,7 +7,7 @@ class DB_Functions {
     //put your code here
     // constructor
     function __construct() {
-        require_once '/db_connect.php';
+        require_once 'db_connect.php';
         // connecting to database
         $this->db = new DB_CONNECT();
         $this->db->connect();
