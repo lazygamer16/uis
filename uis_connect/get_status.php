3f119dbb1a40b03debdf393d1bcdaf6a04351f37
gilbertwong <lazygamer16@live.com>
2015-04-22 16:52:28 +0800

    adding status php

---
+++ uis_connect/get_status.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * Following code will get single news details
+ * A news is identified by news id (nid)
+ */
+
+// array for JSON response
+$response = array();
+
+// include db connect class
+require_once __DIR__ . '/db_connect.php';
+
+// connecting to db
+$db = new DB_CONNECT();
+
+// check for post data
+if (isset($_GET["sid"])) {
+    $sid = $_GET['sid'];
+
+    // get student module status
+    $result = mysql_query("SELECT *
+	FROM student
+		INNER JOIN module
+		ON student.student_module=module.module_id WHERE id = $sid");
+
+    if (!empty($result)) {
+        // check for empty result
+        if (mysql_num_rows($result) > 0) {
+
+            $result = mysql_fetch_array($result);
+
+            $stat = array();
+            $stat["sid"] = $result["sid"];
+            $stat["mname"] = $result["module_name"];
+            $stat["sname"] = $result["student_name"];
+            $stat["cname"] = $result["student_course"];
+            // success
+            $response["success"] = 1;
+
+            // user node
+            $response["status"] = array();
+
+            array_push($response["status"], $stat);
+
+            // echoing JSON response
+            echo json_encode($response);
+        } else {
+            // no status found
+            $response["success"] = 0;
+            $response["message"] = "No student status found";
+
+            // echo no users JSON
+            echo json_encode($response);
+        }
+    } else {
+        // no status found
+        $response["success"] = 0;
+        $response["message"] = "No status 2 found";
+
+        // echo no users JSON
+        echo json_encode($response);
+    }
+} else {
+    // required field is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field(s) is missing";
+
+    // echoing JSON response
+    echo json_encode($response);
+}
+?>
