2fae11f3f18cde95a9383df2f10892f9d2d69e57
gilbertwong <lazygamer16@live.com>
2015-04-25 12:18:25 +0800

    tweak code

--- uis_connect/logout.php
+++ uis_connect/logout.php
@@ -9,9 +9,9 @@
 $response = array();
  
 // check for required fields
-if (isset($_POST['uid'])) {
+if (isset($_POST['sid'])) {
  
-	$uid = $_POST['uid'];
+	$sid = $_POST['sid'];
  
     // include db connect class
     require_once __DIR__ . '/db_connect.php';
@@ -20,7 +20,10 @@ if (isset($_POST['uid'])) {
     $db = new DB_CONNECT();
  
     // mysql update row with matched pid
-    $result = mysql_query("UPDATE user SET login = '0' WHERE user_id = $uid");
+    $result = mysql_query("UPDATE user join student
+							ON student.user_id = user.user_id
+							SET login = 0
+							WHERE student.student_id = $sid");
 
     // check if row inserted or not
     if ($result) {
